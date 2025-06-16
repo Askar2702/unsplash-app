bdb443ed26b572a0d4808f0c8f9313c02c2ed4ff
Askar2702 <56299054+Askar2702@users.noreply.github.com>
2025-06-17 03:08:36 +0500

    исправление роутера

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Photo from "./pages/Photo";
@@ -7,14 +7,14 @@ import Favorites from "./pages/Favorites";
 
 function App() {
   return (
-    <BrowserRouter>
+    <>
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/photo/:id" element={<Photo />} />
         <Route path="/favorites" element={<Favorites />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import { StrictMode } from "react";
-import "./index.css";
-import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom"; // <--- Важно!
+import App from "./App.jsx";
+import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/unsplash-app/">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
